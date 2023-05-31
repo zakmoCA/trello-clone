5d984218008724ca229299da5c23cc9cd11c795b
zakmoCA <13905@coderacademy.edu.au>
2023-05-31 14:35:57 +1000

    table seeded

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from datetime import date
 
 app = Flask(__name__)
 
@@ -8,17 +9,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameg
 db = SQLAlchemy(app)
 
 class Card(db.Model):
+    
+    __tablename__ = 'cards'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
-    date = db.Column(db.Date())
+    date_created = db.Column(db.Date())
 
 @app.cli.command('create')
 def create_db():
     db.create_all()
     print('Tables created successfully')
 
+@app.cli.command('seed')
+def seed_db():
+    # Create an instance of the Card model in memory
+    card = Card(
+      title = 'Start the project',
+      description = 'Stage 1 - Create an ERD',
+      date_created = date.today()
+    )
+
+    # Truncate the Card table
+    db.session.query(Card).delete()
+    
+    # Add the card to the session (transaction)
+    db.session.add(card)
+
+    # Commit the transaction to the database
+    db.session.commit()
+    print('Models seeded')
+
+
+
 print(db.__dict__)
+
 @app.route('/')
 def index():
     return 'Hello world!'
