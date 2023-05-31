93119fd2c70e063023daca15b3810a1809f5efd2
zakmoCA <13905@coderacademy.edu.au>
2023-05-31 12:09:19 +1000

    created card model

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psychopg2://trello_dev:spameggs123@localhost:5432'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello_clone'
 
+db = SQLAlchemy(app)
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.Text())
+    date = db.Column(db.Date())
+
+@app.cli.command('create')
+def create_db():
+    db.create_all()
+    print('Tables created successfully')
+
+print(db.__dict__)
 @app.route('/')
 def index():
     return 'Hello world!'
