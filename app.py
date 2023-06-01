363a606e2e752be8a52a2fcdc0fac0dd131574b3
zakmoCA <13905@coderacademy.edu.au>
2023-06-01 11:50:45 +1000

    card status

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import date
+import json
 
 app = Flask(__name__)
 
@@ -14,27 +15,45 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
+    status = db.Column(db.String(30))
     date_created = db.Column(db.Date())
 
 @app.cli.command('create')
 def create_db():
+    db.drop_all()
     db.create_all()
     print('Tables created successfully')
 
 @app.cli.command('seed')
 def seed_db():
     # Create an instance of the Card model in memory
-    card = Card(
-      title = 'Start the project',
-      description = 'Stage 1 - Create an ERD',
-      date_created = date.today()
-    )
-
+    cards = [
+        Card(
+        title = 'Start the project',
+        description = 'Stage 1 - Create an ERD',
+        # Create an ERC
+        status = 'Done',
+        date_created = date.today()
+        ),
+        Card(
+        title = 'ORM Queries',
+        description = 'Stage 2 - Implement several queries',
+        status = 'In progress',
+        date_created = date.today()
+        ),
+        Card(
+        title = 'Marshmallow',
+        description = 'Stage 3 - Implement jsonify of mdoels',
+        status = 'In progress',
+        date_created = date.today()
+        ),
+    ]
     # Truncate the Card table
     db.session.query(Card).delete()
     
     # Add the card to the session (transaction)
-    db.session.add(card)
+    db.session.add_all(cards)
+    
 
     # Commit the transaction to the database
     db.session.commit()
@@ -42,7 +61,13 @@ def seed_db():
 
 
 
-print(db.__dict__)
+@app.cli.command('all_cards')
+def all_cards():
+    # select * from cards;
+    stmt = db.select(Card).where(Card.status != 'Done', Card.id > 2)
+    cards = db.session.scalars(stmt).all()
+    for card in cards:
+        print(card.__dict__)
 
 @app.route('/')
 def index():
