69a07befc448b34654d3f3e6f0a73e7d8fa6ca5d
zakmoCA <13905@coderacademy.edu.au>
2023-06-07 21:02:42 +1000

    flask lessons up to date

--- app.py
+++ app.py
@@ -2,14 +2,35 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
+from sqlalchemy.exc import IntegrityError
+from flask_jwt_extended import JWTManager, create_access_token
+from datetime import timedelta
 
 
 app = Flask(__name__)
+
+app.config['JWT_SECRET_KEY'] = 'Ministry of Silly Walks' 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:spameggs123@localhost:5432/trello_clone'
 
 db = SQLAlchemy(app)
 ma= Marshmallow(app)
+bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'email', 'password', 'is_admin')
 
 class Card(db.Model):
     
@@ -33,6 +54,19 @@ def create_db():
 
 @app.cli.command('seed')
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('spinynorman').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='cleese@spam.com',
+            password=bcrypt.generate_password_hash('tisbutascratch').decode('utf-8')
+        )
+    ]
+    
     # Create an instance of the Card model in memory
     cards = [
         Card(
@@ -57,18 +91,53 @@ def seed_db():
     ]
     # Truncate the Card table
     db.session.query(Card).delete()
+    db.session.query(User).delete()
     
     # Add the card to the session (transaction)
     db.session.add_all(cards)
+    db.session.add_all(users)
     
 
     # Commit the transaction to the database
     db.session.commit()
     print('Models seeded')
 
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        # Parse, sanitize and validate the incoming JSON data
+        # via the schema
+        user_info = UserSchema().load(request.json)
+        # Create a new User model instance with the schema data
+        user = User(
+            email=user_info['email'],
+            password=bcrypt.generate_password_hash(user_info['password']).decode('utf-8'),
+            name=user_info['name']
+        )
+
+        # Add and commit the new user
+        db.session.add(user)
+        db.session.commit()
+
+        # Return the new user, excluding the password
+        return UserSchema(exclude=['password']).dump(user), 201
+    except IntegrityError:
+        return {'error': 'Email address already in use'}, 409
+    
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        stmt = db.select(User).filter_by(email=request.json['email'])
+        user = db.session.scalar(stmt)
+        if user and bcrypt.check_password_hash(user.password, request.json['password']):
+            token = create_access_token(identity=user.email, expires_delta=timedelta(days=1))
+            return {'token': token, 'user': UserSchema(exclude=['password']).dump(user)}
+        else:
+            return {'error': 'Invalid email address or password'}, 401
+    except KeyError:
+        return {'error': 'Email and password are required'}, 400
 
 
-@app.cli.command('all_cards')
 @app.route('/cards')
 def all_cards():
     # select * from cards;
