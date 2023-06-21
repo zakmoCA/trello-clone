3d09480686086272e67b15a686a5abc9068e3d09
zakmoCA <13905@coderacademy.edu.au>
2023-06-22 00:01:38 +1000

    routes back in order

--- app.py
+++ app.py
@@ -20,6 +20,10 @@ app.config['JWT_SECRET_KEY'] = environ.get('JWT_KEY')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URI')
 
+db.init_app(app)
+ma.init_app(app)
+jwt.init_app(app)
+bcrypt.init_app(app)
 
 def admin_required():
     user_email = get_jwt_identity()
